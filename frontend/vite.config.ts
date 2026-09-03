52d2226a7c8aba61f66f4d02919b8602a6e88642
Samuel Santana <samuelsantanadasilva8@gmail.com>
2026-09-03 09:03:06 -0300

    :sparkles: adjustment config for project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // tailwindcss()
+    tailwindcss(),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
